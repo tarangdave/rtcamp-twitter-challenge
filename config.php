72b2ba45160d4b8407404316bb64fb6338c2c977
Tarang Dave <tarangadave@gmail.com>
2016-09-21 21:57:33 +0530

    updated config

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 /* configuration */
 
-define('CONSUMER_KEY', 'i0qrMn30UPcnc7ytGyYVv86kg');
-define('CONSUMER_SECRET', 'qQMogHQdET3U78NiuA6L132omlBEAy4Z5qH2BjXrK0JRiRlkQW');
-define('OAUTH_CALLBACK', 'http://127.0.0.1/tr/redirect.php'); //for localhost
+define('CONSUMER_KEY', 'Ge3XmXMx1FI7O9vwbP6yUC20l');
+define('CONSUMER_SECRET', 'lmbxD6EyX6bW232Jywy4tM5r08vX6IO3OOIsdzGg4byFRQve32');
+define('OAUTH_CALLBACK', 'http://rttwitter123.byethost24.com/redirect.php'); //for localhost
 
 
 ?>
